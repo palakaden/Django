566db2c5f6da11ee41a34b0d58aa67e9ae69a6c7
palakaden <thomasajay22@gmail.com>
2024-02-19 07:25:21 +0530

    edit

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
 from Admin import views
+app_name="webadmin"
 urlpatterns = [
-     path("district/",views.district),
-     path("category/",views.category),
+     path("district/",views.district,name="district"),
+     path("category/",views.category,name="category"),
+     path("Deletedistrict/<int:did>",views.deldistrict,name="deldistrict"),
+     path("Editdistrict/<int:eid>",views.editdistrict,name="editdistrict"),
+     path("Deletecategory/<int:did>",views.delcategory,name="delcategory"),
+     path("Editcategory/<int:eid>",views.editcategory,name="editcategory"),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from Admin.models import*
 # Create your views here.
 
@@ -8,7 +8,7 @@ def district(request):
         tbl_district.objects.create(district_name=request.POST.get("dst")),
         return render(request,"Admin/district.html",{"district": districtdata})
     else:
-        return render(request,"Admin/district.html")
+        return render(request,"Admin/district.html",{"district": districtdata})
 
 def category(request):
     categorydata=tbl_category.objects.all()
@@ -16,5 +16,30 @@ def category(request):
         tbl_category.objects.create(category_name=request.POST.get("cat")),
         return render(request,"Admin/category.html",{"category": categorydata})
     else:
-        return render(request,"Admin/category.html")
+        return render(request,"Admin/category.html",{"category": categorydata})
 
+def deldistrict(request,did):
+    tbl_district.objects.get(id=did).delete()
+    return redirect("webadmin:district")
+
+def editdistrict(request,eid):
+    data=tbl_district.objects.get(id=eid)
+    if request.method=="POST":
+        data.district_name=request.POST.get("dst")
+        data.save()
+        return redirect("webadmin:district")
+    else:
+        return render(request,"Admin/district.html",{"edit": data})
+    
+def delcategory(request,did):
+    tbl_category.objects.get(id=did).delete()
+    return redirect("webadmin:category")
+
+def editcategory(request,eid):
+    data=tbl_category.objects.get(id=eid)
+    if request.method=="POST":
+        data.category_name=request.POST.get("cat")
+        data.save()
+        return redirect("webadmin:category")
+    else:
+        return render(request,"Admin/category.html",{"edit": data})
